23c88e3552f99062b36db5a97af0c129cdbeb46d
wjkaufman <will@wjkaufman.com>
2019-02-07 20:15:05 -0500

    add navbar, routes for app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BequestsComponent } from './bequests/bequests.component';
+import { PeopleComponent } from './people/people.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'bequests', component: BequestsComponent },
+  { path: 'people', component: PeopleComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
